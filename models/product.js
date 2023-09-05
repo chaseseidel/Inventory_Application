035ee6a4d19463115299eb5058ff226037b90bc1
chaseseidel <chaseseidel16@gmail.com>
2023-09-05 11:11:47 -0700

    Remove length requirement on description

--- models/product.js
+++ models/product.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
-  name: { type: String, required: true, maxLength: 100 },
-  description: { type: String, minLength: 3, maxLength: 500 },
+  name: { type: String, required: true },
+  description: String,
   category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
   price: { type: Number, required: true },
   number_in_stock: { type: Number, min: 0, required: true },
