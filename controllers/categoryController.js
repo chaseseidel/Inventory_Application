7f495a662ac3df9345a731c6f3b53a707a019bfe
chaseseidel <chaseseidel16@gmail.com>
2023-09-05 11:12:04 -0700

    Implement create GET and POST

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,7 @@
 const Category = require("../models/category");
+const Product = require("../models/product");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
   res.render("index", {
@@ -16,16 +18,48 @@ exports.category_list = asyncHandler(async (req, res, next) => {
 });
 
 exports.category_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Category detail: ${req.params.id}`);
+  const [category, allProductsInCategory] = await Promise.all([
+    Category.findById({ _id: req.params.id }).exec(),
+    Product.find({ category: req.params.id }, { name: 1 }).exec(),
+  ]);
+  res.render("category_detail", {
+    category: category,
+    product_list: allProductsInCategory,
+  });
 });
 
 exports.category_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category create GET");
+  res.render("category_form", {
+    title: "Add Category",
+  });
 });
 
-exports.category_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category create POST");
-});
+exports.category_create_post = [
+  body("name", "Name must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("category_form", {
+        category: category,
+        errors: errors.array(),
+      });
+    } else {
+      await category.save();
+      res.redirect(category.url);
+    }
+  }),
+];
 
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Category delete GET");
