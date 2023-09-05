02e776e83e2dda6cc74d61fe19b31bca648b618f
chaseseidel <chaseseidel16@gmail.com>
2023-09-04 20:43:36 -0700

    Add routes for CRUD of products and categories

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const category_controller = require("../controllers/categoryController");
+const product_controller = require("../controllers/productController");
+
+router.get("/", category_controller.index);
+
+//CATEGORY ROUTES
+
+router.get("/category/create", category_controller.category_create_get);
+
+router.post("/category/create", category_controller.category_create_post);
+
+router.get("/category/:id/delete", category_controller.category_delete_get);
+
+router.post("/category/:id/delete", category_controller.category_delete_post);
+
+router.get("/category/:id/update", category_controller.category_update_get);
+
+router.post("/category/:id/update", category_controller.category_update_post);
+
+router.get("/category/:id", category_controller.category_detail);
+
+router.get("/categories", category_controller.category_list);
+
+//PRODUCT ROUTES
+
+router.get("/product/create", product_controller.product_create_get);
+
+router.post("/product/create", product_controller.product_create_post);
+
+router.get("/product/:id/delete", product_controller.product_delete_get);
+
+router.post("/product/:id/delete", product_controller.product_delete_post);
+
+router.get("/product/:id/update", product_controller.product_update_get);
+
+router.post("/product/:id/update", product_controller.product_update_post);
+
+router.get("/product/:id", product_controller.product_detail);
+
+router.get("/products", product_controller.product_list);
 
 module.exports = router;
