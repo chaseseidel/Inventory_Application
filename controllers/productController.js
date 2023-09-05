5cc6998f41bf5be6a7e5eda819182b76a162e159
chaseseidel <chaseseidel16@gmail.com>
2023-09-05 12:19:01 -0700

    Implement update GET and POST

--- controllers/productController.js
+++ controllers/productController.js
@@ -79,9 +79,56 @@ exports.product_delete_post = asyncHandler(async (req, res, next) => {
 });
 
 exports.product_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Product update GET");
+  const [product, allCategories] = await Promise.all([
+    Product.findById({ _id: req.params.id }).exec(),
+    Category.find().sort({ name: 1 }).exec(),
+  ]);
+
+  res.render("product_form", {
+    title: "Update Product: " + product.name,
+    product: product,
+    category_list: allCategories,
+  });
 });
 
-exports.product_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Product update POST");
-});
+exports.product_update_post = [
+  body("name", "Name must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("category", "Category must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Price must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("number_in_stock", "Stock must not be empty")
+    .isNumeric({ gt: 0 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const product = new Product({
+      name: req.body.name,
+      category: req.body.category,
+      price: parseFloat(req.body.price),
+      number_in_stock: req.body.number_in_stock,
+      _id: req.params.id,
+    });
+
+    if (req.body.description) product.description = req.body.description;
+
+    if (!errors.isEmpty()) {
+      const allCategories = await Category.find().sort({ name: 1 }).exec();
+      res.render("product_form", {
+        product: product,
+        category_list: allCategories,
+        errors: errors.array(),
+      });
+    } else {
+      const updatedProduct = await Product.findByIdAndUpdate(
+        req.params.id,
+        product,
+        {}
+      );
+      res.redirect(updatedProduct.url);
+    }
+  }),
+];
